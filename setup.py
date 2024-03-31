3731d53317d29c3fb317cab30f3785c0b242057e
zlElo <mail@zlelo.de>
2024-03-31 20:09:20 +0200

    change setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='loggius-maximus',
+    name='loggiusm',
     packages=find_packages(include=['loggiusm']),
     version='0.1.1',
     description='Loggius-maximus is a damn easy logging tool for python',
