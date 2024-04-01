5f378fa678f2599956cfe32d9e070aca547bf404
zlElo <mail@zlelo.de>
2024-04-01 09:44:12 +0200

    add better docstrings

--- loggiusm/logger.py
+++ loggiusm/logger.py
@@ -17,12 +17,26 @@ class LogMx():
     """
     def debug(log_msg, file_path=None, highliting=True, print_out=True):
         """
-        Writes logs, optionally to a file
-        -> prints to stdout by default
-        -> highlighting by default
+        Writes logs, optionally to a file.
+
+        Parameters
+        ----------
+        log_msg : str
+            The message to be logged.
+        file_path : str, optional
+            The path to the file where the log should be saved, by default None
+        highliting : bool, optional
+            Whether the DEBUG tag should be highlighted with ANSI escape sequences, by default True
+        print_out : bool, optional
+            Whether the log should be printed to the stdout, by default True
+
+        Returns
+        -------
+        str
+            The log string if print_out is set to False
         """
         current_date = datetime.datetime.now()
-        if highliting == True:
+        if highliting:
             log_string = f'\033[92m[DBG]\033[0m [{current_date.strftime("%d/%m/%Y %H:%M:%S")}] {log_msg}'
         else:
             log_string = f'[DBG] [{current_date.strftime("%d/%m/%Y %H:%M:%S")}] {log_msg}'
@@ -32,17 +46,35 @@ class LogMx():
             with open(file_path, 'a') as log:
                 log.write(f'\n[DBG] [{current_date.strftime("%d/%m/%Y %H:%M:%S")}] {log_msg}')
 
-        if print_out == True:
+        if print_out:
             print(log_string, file=sys.stdout)
         else:
             return log_string
 
     def error(log_msg, file_path=None, highlighting=True, print_out=True, line=None):
         """
-        Writes errors, optionally to a file
-        -> prints to stdout by default
-        -> highlighting by default
-        -> line expects list with [line number, file name, function name]
+        Writes errors, optionally to a file.
+
+        Parameters
+        ----------
+        log_msg : str
+            The message to be logged.
+        file_path : str, optional
+            The path to the file where the log should be saved, by default None
+        highlighting : bool, optional
+            Whether the ERROR tag should be highlighted with ANSI escape sequences,
+            by default True
+        print_out : bool, optional
+            Whether the log should be printed to the stdout, by default True
+        line : List[str], optional
+            A list containing information about the line where the error occurred,
+            by default None. The list should contain [line number, file name,
+            function name].
+
+        Returns
+        -------
+        str or None
+            The log string if print_out is set to False
         """
 
         # get datetime
@@ -64,6 +96,17 @@ class LogMx():
     
     @staticmethod
     def catch(file_path=None):
+        """
+        A decorator that catches exceptions and logs them with LogMx.error
+
+        Parameters
+        ----------
+        file_path : str, optional
+
+        Returns
+        -------
+        decorator
+        """
         def decorator(func):
             def wrapper(*args, **kwargs):
                 try:
