39e13596942747fed73ea7fe99b0e9286f2e86ca
zlElo <mail@zlelo.de>
2024-04-01 10:27:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ setup(
     name='froggius',
     packages=find_packages(include=['froggius']),
     description='Froggius is a dumb easy logging tool for python',
-    version='0.1.2',
+    version='{{VERSION_PLACEHOLDER}}',
     author='zlElo',
+    author_email="mail@zlelo.de",
+    url = "https://github.com/zlElo/Froggius",
+    keywords=['pypi', 'cicd', 'python'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
 )
