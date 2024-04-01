c2536fa380768132806ded628dfac11a0301478f
zlElo <mail@zlelo.de>
2024-04-01 10:08:10 +0200

    changed weird version stuff

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='froggius',
     packages=find_packages(include=['froggius']),
-    version='Froggius is a dumb easy logging tool for python',
+    description='Froggius is a dumb easy logging tool for python',
+    version='0.1.2',
     author='zlElo',
 )
