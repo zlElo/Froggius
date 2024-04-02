f85f42c5a1b2fe37b64285e688c1a13300386fd7
zlElo <mail@zlelo.de>
2024-04-02 15:53:31 +0200

    easier import statement

--- examples.py
+++ examples.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from froggius.logger import Froggius
+from froggius import Froggius
 
 # ----- Logging/Debugging -----
 # Example normal logging
